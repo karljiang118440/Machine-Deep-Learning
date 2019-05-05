d1a9d18e257ca67e441a74ea745baa920ebd147d
karljiang118440 <jiang_chaoqing@126.com>
2019-05-05 11:48:24 +0800

    add tf

--- TensorFlow practise/Chapter6/example6.3.1.py
+++ TensorFlow practise/Chapter6/example6.3.1.py
@@ -1,3 +1,14 @@
 
 
-filter_weights=tf.get
+filter_weights=tf.get_variable(
+	                          )
+
+
+biases=tf.get_variable('biases',[16],initializer=tf.constant_initializer(0.1))
+
+conv = tf.nn.conv2d(input,filter_weights,strides=[1,1,1,1],padding='SAME')
+
+biases=tf.nn.bias_add(conv,biases)
+
+actived_conv=tf.nn.relu(bias)
+

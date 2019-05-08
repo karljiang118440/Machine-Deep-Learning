ea42dd678544eedd1224e7851dc0321a7aa63f0d
karljiang118440 <jiang_chaoqing@126.com>
2019-05-08 16:01:18 +0800

    example_10.3: keras

---
+++ TensorFlow practise/Chapter10/example_10.3.py
@@ -0,0 +1,13 @@
+
+import tensorflow as tf
+q=tf.FIFOQueue(2,"int32")
+init =q.enqueue_many(([0,10],))
+x=q.dequeue()
+y=x+1
+q_inc=q.enqueue([y])
+
+with tf.Session() as sess:
+    init.run()
+    for _ in range(5):
+        v,_=sess.run([x,q_inc])
+        print (v)

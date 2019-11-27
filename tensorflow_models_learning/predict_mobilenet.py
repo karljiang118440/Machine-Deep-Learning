b9cabbb8057d5f933ffc2567bf6d57b21d727347
karljiang118440 <jiang_chaoqing@126.com>
2019-11-27 10:50:42 +0800

    mobilenet parametets corrrect

--- tensorflow_models_learning/predict_mobilenet.py
+++ tensorflow_models_learning/predict_mobilenet.py
@@ -6,7 +6,9 @@ import pdb
 import cv2
 import os
 import glob
-import slim.nets.inception_v3 as inception_v3
+
+#karl-1-20191127:修改原先 inceptionv3为 mobilenet_v1
+import slim.nets.mobilenet_v1 as mobilenet_v1
 
 from create_tf_record import *
 import tensorflow.contrib.slim as slim
@@ -19,8 +21,9 @@ def  predict(models_path,image_dir,labels_filename,labels_nums, data_format):
     input_images = tf.placeholder(dtype=tf.float32, shape=[None, resize_height, resize_width, depths], name='input')
 
     #其他模型预测请修改这里
-    with slim.arg_scope(inception_v3.inception_v3_arg_scope()):
-        out, end_points = inception_v3.inception_v3(inputs=input_images, num_classes=labels_nums, dropout_keep_prob=1.0, is_training=False)
+    with slim.arg_scope(mobilenet_v1.mobilenet_v1_arg_scope()):
+        out, end_points = mobilenet_v1.mobilenet_v1(inputs=input_images, num_classes=labels_nums, dropout_keep_prob=1.0, is_training=False)
+
 
     # 将输出结果进行softmax分布,再求最大概率所属类别
     score = tf.nn.softmax(out,name='pre')
@@ -46,11 +49,15 @@ if __name__ == '__main__':
     class_nums=5
     image_dir='test_image'
     labels_filename='dataset/label.txt'
-    models_path='models/model.ckpt-10000'
+
+   # karl-3-20191127:模型改为20000  
+    models_path='models/model.ckpt-20000'
 
     batch_size = 1  #
-    resize_height = 299  # 指定存储图片高度
-    resize_width = 299  # 指定存储图片宽度
+
+   # karl-2-20191127:修改其尺寸大小
+    resize_height = 224  # 指定存储图片高度 
+    resize_width = 224  # 指定存储图片宽度
     depths=3
     data_format=[batch_size,resize_height,resize_width,depths]
     predict(models_path,image_dir, labels_filename, class_nums, data_format)
